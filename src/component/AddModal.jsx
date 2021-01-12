fad6378e4f5639e9950250b82d1e25263deca3d0
Rabia Vural <rvural@mscteknoloji.com>
2021-01-13 00:03:06 +0300

    update

--- src/component/AddModal.jsx
+++ src/component/AddModal.jsx
@@ -8,35 +8,40 @@ export function AddModal(props) {
     }
     const [modalData, setModal] = useState({
         id: 0,
-        date: "dd/mm/yyyy",
+        date: null,
         value: "",
     })
 
     function saveAdd() {
-        var idRandom=Math.floor(Math.random() * 101)
-        setModal({...modalData,id:idRandom})
-        var copyData = JSON.parse(JSON.stringify(props.todoList))
-        if (props.returnData) {
-            copyData.push(modalData);
-            props.returnData(copyData);
-            handleClose();
+        if (modalData.date === null || modalData.value==="") {
+            alert("Please make sure all fields are filled.")
+        } else {
+            var idRandom = Math.floor(Math.random() * 101)
+            setModal({ ...modalData, id: idRandom })
+            var copyData = JSON.parse(JSON.stringify(props.todoList))
+            if (props.returnData) {
+                copyData.push(modalData);
+                props.returnData(copyData);
+                handleClose();
+            }
         }
+
     }
     return (
         <div className={showHideClassName}>
             <div className="modal-main">
                 <p className="modal-text-title">Title</p>
-                <input 
-                    type="text" 
-                    value={modalData.value} 
-                    onChange={e => setModal({ ...modalData, value: e.target.value })} 
+                <input
+                    type="text"
+                    value={modalData.value}
+                    onChange={e => setModal({ ...modalData, value: e.target.value })}
                     className="input-modal-text"
                 />
                 <p className="modal-text-date">Date</p>
-                <input 
-                    type="date" 
-                    value={modalData.date} 
-                    onChange={e => setModal({ ...modalData, date: e.target.value })} 
+                <input
+                    type="date"
+                    value={modalData.date}
+                    onChange={e => setModal({ ...modalData, date: e.target.value })}
                     className="input-modal-text-2"
                 />
 
